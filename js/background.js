d0ff86edd8fa2508f08ef3ca5f1f9977ee516fe8
polzoom <polzoom@mail.ru>
2016-07-04 15:32:51 +0300

    extend life period of token

--- js/background.js
+++ js/background.js
@@ -7,7 +7,7 @@ localStorage.tokenUpdatedAt = '';
  * @returns {boolean}
  */
 function isTokenExpired() {
-  return Date.now() - parseInt(localStorage.tokenUpdatedAt, 10) >= 24 * 60 * 60 * 1000;
+  return Date.now() - parseInt(localStorage.tokenUpdatedAt, 10) >= 7 * 24 * 60 * 60 * 1000;
 }
 
 chrome.extension.onConnect.addListener(function(port) {
@@ -35,12 +35,12 @@ chrome.extension.onConnect.addListener(function(port) {
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   var url = tab.url;
   if (url && changeInfo.status == 'complete') {
-    TOCAT_TOOLS.updateIcon(url.split('?')[0]);
+    TOCAT_TOOLS.updateIcon(url.split('#')[0]);
   }
 });
 
 chrome.tabs.onActivated.addListener(function() {
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    TOCAT_TOOLS.updateIcon(tabs[0].url.split('?')[0]);
+    TOCAT_TOOLS.updateIcon(tabs[0].url.split('#')[0]);
   });
 });
